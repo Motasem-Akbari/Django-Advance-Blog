68017a20d37abcd5b3500f39f3a86bf04707f28e
Motasem Akbari <motasemakbari@gmail.com>
2025-08-20 12:33:57 +0330

    Add Profile model, signals, and admin customizations
    - Created Profile model linked to custom User
    - Added signals to automatically create/update Profile on User save
    - Customized admin forms and registered CustomUserAdmin
    - Added STATIC and MEDIA directory structure for handling assets
    - Configured admin.py with fieldsets, add_fieldsets, and display options

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 # Create your models here.
 
 
@@ -59,3 +62,25 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+
+class Profile(models.Model):
+    '''
+    User profile model
+    '''
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    image = models.ImageField(blank=True, null=True)
+    discretion = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.user.email}"
+    
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
