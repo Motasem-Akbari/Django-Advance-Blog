90e0ea5fb49b3841eb1072583f8f945435c7a409
Motasem Akbari <motasemakbari@gmail.com>
2025-08-21 18:55:24 +0330

    Add FBV vs CBV practice example
    - Created a simple Function Based View (hello_fbv) that returns a text response.
    - Implemented a Class Based View (HelloCBV) using Django's View class.
    - Updated urls.py to map /fbv/ and /cbv/ endpoints.
    - This demonstrates the structural difference between FBV and CBV.

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def indexView(request):
+    name = "Motasem"
+    context = {"name": name}
+    return render(request, "index.html", context)
