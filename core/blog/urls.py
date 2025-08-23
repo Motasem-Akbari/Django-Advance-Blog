e5be6edcf3189a0e2520bd68dabe83f1fc43ea9b
Motasem Akbari <motasemakbari@gmail.com>
2025-08-23 12:57:43 +0330

    Add CBV (IndexView) and RedirectView with updated URLs
    - Added IndexView as a class-based view to render index.html with name and posts context
    - Implemented RedirectToMaktab class-based redirect view
    - Updated urls.py:
      - Registered cbv-index with IndexView
      - Added redirect path with pk parameter for RedirectToMaktab
    - Left function-based views as comments for comparison with CBV

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import indexView
+# from .views import indexView
+from . import views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
+app_name = "blog"
 
 urlpatterns = [
-    path("fbv-index", indexView, name="fbv-index"),
-    path("cbv-index", TemplateView.as_view(template_name="index.html",extra_context={"name":"Motasem"})),
+    # path("fbv-index", views.indexView, name="fbv-index"),
+    # path("cbv-index", TemplateView.as_view(template_name="index.html",extra_context={"name":"Motasem"})),
+    path("cbv-index", views.IndexView.as_view(), name='cbv-index'),
+    path("go-to-maktabkhooneh/<int:pk>/",views.RedirectToMaktab.as_view(),name="redirect-to-maktabkhooneh"),
+
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+from .models import Post
 # Create your views here.
 
-
+# function based view
+'''
 def indexView(request):
+    """
+    a function based view that renders the index.html template with a name context
+    """
     name = "Motasem"
     context = {"name": name}
     return render(request, "index.html", context)
+'''
+class IndexView(TemplateView):
+    """
+    a class based view that renders the index.html template with a name context
+    """
+    template_name = "index.html"
+    
+    def get_context_data(self, **kwargs):
+        condext = super().get_context_data(**kwargs)
+        condext["name"] = "Motasem"
+        condext["posts"] = Post.objects.all()
+        return condext
+    
+
+
+''' FBV for redirect view
+
+from django.shortcuts import redirect
+def redirectToMakatab(request):
+    """
+    a function based view that redirects to maktabkhooneh.org
+    """
+    return redirect("https://www.maktabkhoneh.org/")
+    
+'''
+
+class RedirectToMaktab(RedirectView):
+    url = "https://maktabkhooneh.org/"
+    
