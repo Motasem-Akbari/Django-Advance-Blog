b4ffbf3209e9551bfa1fcda319b0f19eb214a116
Motasem Akbari <motasemakbari@gmail.com>
2025-10-03 17:41:46 +0330

    Refactor models into separate files
    - Reorganized models.py into a models package
    - Created separate files for each model (user, profile, token, etc.)
    - Added __init__.py to import all models for Django recognition
    - Improves readability and maintainability of large projects
    
    - and urls...

---
+++ core/accounts/models/profile.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .users import User
+
+
+
+class Profile(models.Model):
+    '''
+    User profile model
+    '''
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    image = models.ImageField(blank=True, null=True)
+    discretion = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.user.email}"
+    
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
