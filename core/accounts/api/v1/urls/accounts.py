24e6a3313ebc65236197bba23ef98d393aa33713
Motasem Akbari <motasemakbari@gmail.com>
2025-10-03 19:08:06 +0330

    Integrate SMTP4Dev in Docker setup
    - Configured SMTP4Dev service in Docker for local email testing
    - Enabled sending and viewing emails without external SMTP server
    - Updated Docker Compose configuration accordingly
    - Facilitates development and debugging of email-related features

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -10,6 +10,13 @@ urlpatterns = [
     # registration
     path('registration/',views.RegistrationApiView.as_view(),name='registration'),
 
+
+    path('test-email/',views.TestEmailSent.as_view(),name= 'test-email'),
+    # activation
+    # path('activation/confirm/')
+    # resend activation
+    # path('activation/resent/')
+
     # change password
     path('change-password/',views.ChangePasswordApiView.as_view(),name='change-password'),
 
